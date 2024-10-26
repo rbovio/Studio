faa1bc31f22673686c9adbe4c54b7e3b53df2a67
rbovio <rsbovio@gmail.com>
2024-10-26 13:41:57 +0000

    Modify DATABASE settings.py

--- Studio/settings.py
+++ Studio/settings.py
@@ -16,10 +16,6 @@ import os # required for Templates DIRS
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Support env variables from .env file if defined
-from dotenv import load_dotenv
-load_dotenv('/home/dnalab/.local/share/virtualenvs/Studio-_rjAtFXb/.env')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -88,8 +84,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'HOST': 'postgres.railway.internal',
-        'PORT': '5432',
     }
 }
 
