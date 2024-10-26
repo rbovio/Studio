1210ab696056baaa9e5353f3019649d316d240fd
rbovio <rsbovio@gmail.com>
2024-10-26 13:33:19 +0000

    Modify DATABASE settings.py

--- Studio/settings.py
+++ Studio/settings.py
@@ -88,6 +88,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': 'postgre.railway.internal',
+        'PORT': '5432',
     }
 }
 
